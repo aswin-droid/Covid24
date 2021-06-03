d9e7576084725e1c5d4a4468b900a390f320ba9b
Nehaanedath12 <nehamanedath@gmail.com>
2021-06-03 19:19:32 +0530

    LoginActivity Done - Aswin

--- app/src/main/java/com/example/covid24/LoginActivity.java
+++ app/src/main/java/com/example/covid24/LoginActivity.java
@@ -1,33 +1,96 @@
 package com.example.covid24;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
     EditText etEmail, etPass;
     Button btLogin;
     TextView tvRgister;
+    FirebaseFirestore firestore;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         etEmail = findViewById(R.id.etEmail);
-        etPass = findViewById(R.id.etPass);
+        etPass = findViewById(R.id.etPassword);
         btLogin = findViewById(R.id.btLogin);
         tvRgister = findViewById(R.id.tvRegister);
+
+        firestore = FirebaseFirestore.getInstance();
 
         tvRgister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!etEmail.getText().toString().isEmpty()) {
+                    if (!etPass.getText().toString().isEmpty()) {
+
+                        progressDialog = new ProgressDialog(LoginActivity.this);
+                        progressDialog.setMessage("Please wait...");
+                        progressDialog.show();
+
+                        validateLogin();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Please enter Password", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "Please enter Email", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void validateLogin() {
+        firestore.collection("Users").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot documentSnapshot : Objects.requireNonNull(task.getResult())) {
+                        if (documentSnapshot.get("email").toString().equals(etEmail.getText().toString())) {
+                            if (documentSnapshot.get("password").toString().equals(etPass.getText().toString())) {
+
+                                progressDialog.dismiss();
+
+                                Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+                progressDialog.dismiss();
+                Toast.makeText(getApplicationContext(), "Failed caused by " + e.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
